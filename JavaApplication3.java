f70b6d0a049705e81dc3cd78137687723f5638e2
siddhartthecoder <32844499+siddhartthecoder@users.noreply.github.com>
2017-10-20 04:53:08 -0700

    Add files via upload

---
+++ JavaApplication3.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Siddharth1
+ */
+public class JavaApplication3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        simpcalc form= new simpcalc();
+        form.setVisible(true);
+    }
+    
+}
